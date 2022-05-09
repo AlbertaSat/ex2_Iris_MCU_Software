86f02a6eb717172ffe820c4c328546ca32c4e8b4
Liam Droog <droog@ualberta.ca>
2022-05-09 14:26:12 -0600

    Updated command handler in accordance to ICD

--- Core/Inc/command_handler.h
+++ Core/Inc/command_handler.h
@@ -7,7 +7,21 @@
 
 #ifndef INC_COMMAND_HANDLER_H_
 #define INC_COMMAND_HANDLER_H_
+#include "main.h"
+#include "arducam.h"
+#include "SPI_IT.h"
+#include "I2C.h"
+#include "housekeeping.h"
+#define REG_SYS_CTL0 0x3008 /* System Control */
 
-
+void take_image();
+void transfer_image();
+void get_image_length();
+void count_images();
+void sensor_idle();
+void sensor_active();
+void get_housekeeping();
+void update_sensor_I2C_regs();
+void update_current_limits();
 
 #endif /* INC_COMMAND_HANDLER_H_ */
