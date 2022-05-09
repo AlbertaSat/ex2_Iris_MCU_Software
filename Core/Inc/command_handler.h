46989329ee0353b89a5a57a1c39d89df7da5f498
Liam Droog <droog@ualberta.ca>
2022-05-09 09:41:17 -0600

    Update command handler to SPI protocol

---
+++ Core/Inc/command_handler.h
@@ -0,0 +1,13 @@
+/*
+ * command_handler.h
+ *
+ *  Created on: May 9, 2022
+ *      Author: liam
+ */
+
+#ifndef INC_COMMAND_HANDLER_H_
+#define INC_COMMAND_HANDLER_H_
+
+
+
+#endif /* INC_COMMAND_HANDLER_H_ */
---
+++ Core/Src/command_handler.c
@@ -0,0 +1,42 @@
+/*
+ * command_handler.c
+ *
+ *  Created on: May 9, 2022
+ *      Author: liam
+ */
+#include "command_handler.h"
+
+void take_image(){
+	return;
+}
+
+void get_image_length(){
+	return;
+}
+
+void count_images(){
+	return;
+}
+
+void sensor_idle(){
+	return;
+}
+
+void sensor_active(){
+	return;
+}
+
+void get_housekeeping(){
+	return;
+}
+
+
+void update_sensor_I2C_regs(){
+	return;
+}
+
+void update_current_limits(){
+	return;
+}
+
+
