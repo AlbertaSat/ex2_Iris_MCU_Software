9163fbfd3f1fb9eda57815b30a221f5b3af5312e
Liam Droog <droog@ualberta.ca>
2022-05-10 11:03:30 -0600

    Added methods for 8 bit address, 16 bit registers R/W

--- Core/Src/I2C.c
+++ Core/Src/I2C.c
@@ -62,6 +62,18 @@ void i2c2_write8_8(uint8_t addr, uint8_t register_pointer, uint8_t register_valu
 	hi2c_write8_8(hi2c2, addr, register_pointer, register_value);
 	return;
 }
+
+//untested
+uint16_t i2c2_read8_16(uint8_t addr, uint8_t register_pointer){
+	uint16_t val = hi2c_read8_16(hi2c2, addr, register_pointer);
+	return val;
+}
+//untested
+void i2c2_write8_16(uint8_t addr, uint8_t register_pointer, uint16_t register_value){
+	hi2c_write8_16(hi2c2, addr, register_pointer, register_value);
+	return;
+}
+
 // General functions
 void hi2c_read16_8(I2C_HandleTypeDef hi2c, uint8_t addr, uint16_t register_pointer, uint8_t *reg_data)
 {
@@ -110,6 +122,32 @@ void hi2c_write8_8(I2C_HandleTypeDef hi2c, uint8_t addr, uint8_t register_pointe
     if (status != HAL_OK) {
             char buf[64];
             sprintf(buf, "I2C8_8 write to 0x%x failed: 0x%x\r\n", addr, register_pointer);
+            DBG_PUT(buf);
+        }
+}
+
+uint16_t hi2c_read8_16(I2C_HandleTypeDef hi2c, uint8_t addr, uint8_t register_pointer)
+{
+    uint16_t return_value = 0;
+	HAL_StatusTypeDef status = HAL_OK;
+    status = HAL_I2C_Mem_Read(&hi2c, addr << 1, (uint8_t)register_pointer, I2C_MEMADD_SIZE_8BIT, &return_value, 2, 100);
+    if (status != HAL_OK) {
+            char buf[64];
+            sprintf(buf, "I2C8_16 read from 0x%x register 0x%x failed\r\n", addr, register_pointer);
+            DBG_PUT(buf);
+        }
+    return return_value;
+}
+
+void hi2c_write8_16(I2C_HandleTypeDef hi2c, uint8_t addr, uint8_t register_pointer, uint16_t register_value)
+{
+    uint16_t dataBuffer[1];
+	HAL_StatusTypeDef status = HAL_OK;
+    dataBuffer[0] = register_value;
+    status = HAL_I2C_Mem_Write(&hi2c, addr << 1, (uint8_t)register_pointer, I2C_MEMADD_SIZE_8BIT, dataBuffer, 2, 100);
+    if (status != HAL_OK) {
+            char buf[64];
+            sprintf(buf, "I2C8_16 write to 0x%x failed: 0x%x\r\n", addr, register_pointer);
             DBG_PUT(buf);
         }
 }
