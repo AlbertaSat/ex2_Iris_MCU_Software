fe191726176674ee3093435240703d279a04146f
joshdellaz <josh.lazaruk@gmail.com>
2022-06-09 11:53:36 -0600

    Changed i2c 8/8 to use pointers

--- Core/Inc/drivers/i2c/I2C.h
+++ Core/Inc/drivers/i2c/I2C.h
@@ -18,13 +18,13 @@ void rdSensorReg16_8(uint16_t regID, uint8_t *regDat, uint8_t sensor);
 void i2c2_read16_8(uint8_t addr, uint16_t register_pointer, uint8_t *reg_data);
 void i2c2_write16_8(uint8_t addr, uint16_t register_pointer, uint16_t register_value);
 
-uint8_t i2c2_read8_8(uint8_t addr, uint8_t register_pointer);
+void i2c2_read8_8(uint8_t addr, uint8_t register_pointer, uint8_t *reg_data);
 void i2c2_write8_8(uint8_t addr, uint8_t register_pointer, uint8_t register_value);
 
 void hi2c_read16_8(I2C_HandleTypeDef hi2c, uint8_t addr, uint16_t register_pointer, uint8_t *reg_data);
 void hi2c_write16_8(I2C_HandleTypeDef hi2c, uint8_t addr, uint16_t register_pointer, uint16_t register_value);
 
-uint8_t hi2c_read8_8(I2C_HandleTypeDef hi2c, uint8_t addr, uint8_t register_pointer);
+void hi2c_read8_8(I2C_HandleTypeDef hi2c, uint8_t addr, uint8_t register_pointer, uint8_t *reg_data);
 void hi2c_write8_8(I2C_HandleTypeDef hi2c, uint8_t addr, uint8_t register_pointer, uint8_t register_value);
 
 uint16_t i2c2_read8_16(uint8_t addr, uint8_t register_pointer);
