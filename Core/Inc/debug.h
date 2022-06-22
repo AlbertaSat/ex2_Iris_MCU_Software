bb68cceb5477b3f9c455683e86ccbf089843c297
Robert <taylorr@ualberta.ca>
2022-06-22 13:27:12 -0600

    Add varargs to debug output

--- Core/Inc/debug.h
+++ Core/Inc/debug.h
@@ -1,18 +1,7 @@
 #ifndef DEBUG_DEFH
 #define DEBUG_DEFH
 
-#include "stm32l0xx_hal.h"
-#include <stdio.h>
-#include <string.h>
-#include "main.h"
-
-extern UART_HandleTypeDef huart1;
-
-static inline void DBG_PUT(char *str) {
-#ifdef UART_DEBUG
-    HAL_UART_Transmit(&huart1, (uint8_t *)str, strlen(str), 100);
-#endif
-}
+void DBG_PUT(const char *format, ...);
 
 static inline char hex_2_ascii(uint8_t hex) { return (hex < 10) ? '0' + hex : 'a' + (hex - 10); }
 
