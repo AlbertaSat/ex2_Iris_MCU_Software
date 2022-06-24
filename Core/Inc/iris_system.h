ecda96f1deb7f7001d5e3cfd0dbf504fe04ac840
Liam Droog <droog@ualberta.ca>
2022-06-24 10:37:42 -0600

    endif fixed, moved system defines back to main.h

--- Core/Inc/iris_system.h
+++ Core/Inc/iris_system.h
@@ -52,33 +52,13 @@ extern "C" {
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
-void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define USART2_CS1_Pin GPIO_PIN_0
-#define USART2_CS1_GPIO_Port GPIOA
-#define USART2_CS2_Pin GPIO_PIN_1
-#define USART2_CS2_GPIO_Port GPIOA
-#define USART2_MOSI_Pin GPIO_PIN_2
-#define USART2_MOSI_GPIO_Port GPIOA
-#define USART2_MISO_Pin GPIO_PIN_3
-#define USART2_MISO_GPIO_Port GPIOA
-#define USART2_CLK_Pin GPIO_PIN_4
-#define USART2_CLK_GPIO_Port GPIOA
-#define TEST_OUT1_Pin GPIO_PIN_0
-#define TEST_OUT1_GPIO_Port GPIOB
-#define NAND_CS1_Pin GPIO_PIN_12
-#define NAND_CS1_GPIO_Port GPIOB
-#define WP__Pin GPIO_PIN_8
-#define WP__GPIO_Port GPIOA
-#define CAM_EN_Pin GPIO_PIN_11
-#define CAM_EN_GPIO_Port GPIOA
-#define NAND_CS2_Pin GPIO_PIN_12
-#define NAND_CS2_GPIO_Port GPIOA
+
 /* USER CODE BEGIN Private defines */
 #define software_ver 0x13
 #define VIS_SENSOR 0
@@ -98,4 +78,4 @@ void Error_Handler(void);
 }
 #endif
 
-#endif /* __MAIN_H */
+#endif /* IRIS_SYSTEM_H */
