12786693cf8f5c2be31df1adce2f5ffaa6a7f011
Liam Droog <droog@ualberta.ca>
2022-06-29 13:18:31 -0600

    removed duplicate defines

--- Core/Inc/iris_system.h
+++ Core/Inc/iris_system.h
@@ -60,34 +60,17 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define USART2_CS1_Pin GPIO_PIN_0
-#define USART2_CS1_GPIO_Port GPIOA
-#define USART2_CS2_Pin GPIO_PIN_1
-#define USART2_CS2_GPIO_Port GPIOA
-#define USART2_MOSI_Pin GPIO_PIN_2
-#define USART2_MOSI_GPIO_Port GPIOA
-#define USART2_MISO_Pin GPIO_PIN_3
-#define USART2_MISO_GPIO_Port GPIOA
-#define USART2_CLK_Pin GPIO_PIN_4
-#define USART2_CLK_GPIO_Port GPIOA
-#define TEST_OUT1_Pin GPIO_PIN_0
-#define TEST_OUT1_GPIO_Port GPIOB
-#define NAND_CS1_Pin GPIO_PIN_12
-#define NAND_CS1_GPIO_Port GPIOB
-#define WP__Pin GPIO_PIN_8
-#define WP__GPIO_Port GPIOA
-#define CAM_EN_Pin GPIO_PIN_11
-#define CAM_EN_GPIO_Port GPIOA
-#define NAND_CS2_Pin GPIO_PIN_12
-#define NAND_CS2_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
+
 #define software_ver 0x13
 #define VIS_SENSOR 0x3C
 #define NIR_SENSOR 0x3D
 
-
+/* Iris Model Defines --------------------------------------------------------*/
 //#define IRIS_EM
-#define IRIS_FM
+//#define IRIS_FM
+
+#define IRIS_PROTO
 
 #ifdef IRIS_FM
 #define CURRENTSENSE_5V 0x40
@@ -97,6 +80,15 @@ void Error_Handler(void);
 #ifdef IRIS_EM
 #define CURRENTSENSE_5V 0x40
 #endif
+
+#ifdef IRIS_PROTO
+// defines for iris proto
+#endif
+
+
+/* Iris Spec Defines --------------------------------------------------------*/
+//#define format JPEG
+//#define width 1280
 
 
 /* USER CODE END Private defines */
