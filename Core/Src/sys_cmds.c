e3100a68a3bcfc44148fc29f3b6a763ee874a87f
Liam Droog <droog@ualberta.ca>
2022-06-30 13:34:13 -0600

    remove duplicate function

--- Core/Src/sys_cmds.c
+++ Core/Src/sys_cmds.c
@@ -3,64 +3,10 @@
 #include "command_handler.h"
 #include "debug.h"
 
-uint8_t VIS_DETECTED = 0;
-uint8_t NIR_DETECTED = 0;
+
 
 extern I2C_HandleTypeDef hi2c2;
 
-
-/**
- * @brief Resets target sensor to default parameters
- *
- * @param sensor
- */
-void sensor_reset(uint8_t sensor) {
-    char buf[64];
-
-    arducam_wait_for_ready(sensor);
-
-    // Reset the CPLD
-    write_reg(AC_REG_RESET, 1, sensor);
-    HAL_Delay(100);
-    write_reg(AC_REG_RESET, 0, sensor);
-    HAL_Delay(100);
-
-    if (!arducam_wait_for_ready(sensor)) {
-        DBG_PUT("VIS Camera: SPI Unavailable\r\n");
-    } else {
-        DBG_PUT("VIS Camera: SPI Initialized\r\n");
-    }
-
-    // Change MCU mode
-    write_reg(ARDUCHIP_MODE, 0x0, sensor);
-    wrSensorReg16_8(0xff, 0x01, sensor);
-
-    uint8_t vid = 0, pid = 0;
-    rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid, sensor);
-    rdSensorReg16_8(OV5642_CHIPID_LOW, &pid, sensor);
-
-    int detected = VIS_DETECTED;
-    if (sensor == NIR_SENSOR) {
-        detected = NIR_DETECTED;
-    } else if (sensor != VIS_SENSOR) {
-        sprintf(buf, "illegal sensor %d\r\n", sensor);
-        DBG_PUT(buf);
-    }
-
-    if (vid != 0x56 || pid != 0x42) {
-        sprintf(buf, "Camera I2C Address: Unknown\r\nVIS not available\r\n\n");
-        DBG_PUT(buf);
-        detected = 0;
-    } else {
-        detected = 1;
-        format = JPEG;
-        program_sensor(format, sensor);
-        sprintf(buf, "Camera Mode: JPEG\r\n");
-        DBG_PUT(buf);
-    }
-
-    HAL_Delay(1000);
-}
 
 /**
  * @brief Scans I2C2 (internal) bus for devices. Debug purpose only
