ba8632672602b0a29019d28b34d78930c8c6533a
Liam Droog <droog@ualberta.ca>
2022-07-14 11:35:02 -0600

    add CRC to main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -57,6 +57,8 @@ int width = 2592;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+CRC_HandleTypeDef hcrc;
+
 I2C_HandleTypeDef hi2c1;
 I2C_HandleTypeDef hi2c2;
 
@@ -80,6 +82,7 @@ static void MX_SPI1_Init(void);
 static void MX_SPI2_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_CRC_Init(void);
 /* USER CODE BEGIN PFP */
 static void onboot_commands(void);
 /* USER CODE END PFP */
@@ -141,17 +144,14 @@ int main(void) {
     MX_SPI2_Init();
     MX_USART1_UART_Init();
     MX_TIM2_Init();
+    MX_CRC_Init();
     /* USER CODE BEGIN 2 */
     //    NAND_SPI_Init(&hspi2);
 
     uint8_t can_header[CAN_HEADER_LEN];
     uint8_t can_footer[CAN_FOOTER_LEN];
-
-    while (1) {
-        HAL_Delay(3000);
-    }
     onboot_commands();
-    //    init_filesystem();
+    init_filesystem();
     uint8_t obc_cmd;
 
     char cmd[64];
@@ -289,6 +289,34 @@ void SystemClock_Config(void) {
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
         Error_Handler();
     }
+}
+
+/**
+ * @brief CRC Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_CRC_Init(void) {
+
+    /* USER CODE BEGIN CRC_Init 0 */
+
+    /* USER CODE END CRC_Init 0 */
+
+    /* USER CODE BEGIN CRC_Init 1 */
+
+    /* USER CODE END CRC_Init 1 */
+    hcrc.Instance = CRC;
+    hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_ENABLE;
+    hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_ENABLE;
+    hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_NONE;
+    hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_DISABLE;
+    hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_BYTES;
+    if (HAL_CRC_Init(&hcrc) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN CRC_Init 2 */
+
+    /* USER CODE END CRC_Init 2 */
 }
 
 /**
@@ -561,8 +589,8 @@ static void MX_GPIO_Init(void) {
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(USART2_MISO_GPIO_Port, &GPIO_InitStruct);
 
-    /*Configure GPIO pins : TEST_OUT1_Pin CAN_TX_Pin CAN_S_Pin */
-    GPIO_InitStruct.Pin = TEST_OUT1_Pin | CAN_TX_Pin | CAN_S_Pin;
+    /*Configure GPIO pins : TEST_OUT1_Pin CAN_S_Pin */
+    GPIO_InitStruct.Pin = TEST_OUT1_Pin | CAN_S_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -589,6 +617,13 @@ static void MX_GPIO_Init(void) {
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(NAND_CS2_GPIO_Port, &GPIO_InitStruct);
 
+    /*Configure GPIO pin : CAN_TX_Pin */
+    GPIO_InitStruct.Pin = CAN_TX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    HAL_GPIO_Init(CAN_TX_GPIO_Port, &GPIO_InitStruct);
+
     /*Configure GPIO pin : CAN_RX_Pin */
     GPIO_InitStruct.Pin = CAN_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
@@ -609,7 +644,7 @@ void init_filesystem() {
 
 static void onboot_commands(void) {
 
-#ifdef SPI_DEBUG
+    //#ifdef SPI_DEBUG
     GPIO_InitTypeDef GPIO_InitStruct = {0};
     /*Configure GPIO pin */
     GPIO_InitStruct.Pin = ERR_Pin;
@@ -619,20 +654,20 @@ static void onboot_commands(void) {
     HAL_GPIO_Init(ERR_GPIO_Port, &GPIO_InitStruct);
     ERR_GPIO_Port->BSRR = ERR_Pin;
 
-#endif
+    //#endif
 
     HAL_TIM_Base_Start(&htim2);
-    init_filesystem();
-#ifdef CURRENTSENSE_5V
-    init_ina209(CURRENTSENSE_5V);
-#endif // CURRENTSENSE_5V
-    init_temp_sensors();
+    //    init_filesystem();
+    //#ifdef CURRENTSENSE_5V
+    //    init_ina209(CURRENTSENSE_5V);
+    //#endif // CURRENTSENSE_5V
+    //    init_temp_sensors();
 
-#ifdef IRIS_PROTO
-    sensor_togglepower(1);
-    flood_cam_spi();
-    initalize_sensors();
-#endif // IRIS_PROTO
+    //#ifdef IRIS_PROTO
+    //    sensor_togglepower(1);
+    //    flood_cam_spi();
+    //    initalize_sensors();
+    //#endif // IRIS_PROTO
 
 #ifdef UART_DEBUG
     DBG_PUT("-----------------------------------\r\n");
