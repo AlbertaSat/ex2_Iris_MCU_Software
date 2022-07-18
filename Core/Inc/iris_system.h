d8327a47cb7c14c496be7bebf1de8bc5bf32e261
Ron Unrau <rcunrau@ualberta.ca>
2022-07-18 16:06:05 -0600

    fix the SPI def problem

--- Core/Inc/iris_system.h
+++ Core/Inc/iris_system.h
@@ -86,10 +86,8 @@ void Error_Handler(void);
 #endif
 
 /* Iris Spec Defines --------------------------------------------------------*/
-#ifdef SPI_DEBUG
 #define ERR_Pin GPIO_PIN_9
 #define ERR_GPIO_Port GPIOA
-#endif
 
 /* USER CODE END Private defines */
 
