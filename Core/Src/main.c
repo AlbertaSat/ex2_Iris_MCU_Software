e6d973dcffeff63eed23aa15909c5654301c1533
Jenish Patel <jenish@ualberta.ca>
2022-07-21 14:00:58 -0600

    Fix conflicts with main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -459,7 +459,7 @@ static void MX_SPI2_Init(void) {
     hspi2.Init.Direction = SPI_DIRECTION_2LINES;
     hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
     hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
-    hspi2.Init.CLKPhase = SPI_PHASE_2EDGE;
+    hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
     hspi2.Init.NSS = SPI_NSS_SOFT;
     hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
     hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
@@ -682,7 +682,7 @@ static void onboot_commands(void) {
 #else
     DBG_PUT("-----------------------------------\r\n");
     DBG_PUT("Iris Electronics Test Software\r\n"
-            "        SPI v2_sensors v2 (20/07/2022) Edition         \r\n");
+            "        SPI Edition         \r\n");
     DBG_PUT("-----------------------------------\r\n");
 #endif
 }
